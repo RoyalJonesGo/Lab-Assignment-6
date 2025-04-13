b9a302b832cefc908d7b9e60416766f14434252c
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-04-13 23:37:26 +0800

    Create Thesis.java

---
+++ Thesis.java
@@ -0,0 +1,11 @@
+public class Thesis implements LibraryResource{
+    private String title;
+
+    public Thesis(String title){
+        this.title = title;
+    }
+    @Override
+    public String getTitle(){
+        return title;
+    }
+}
