5cc2276b78f501dce0a4d94a5aa9e198df81b126
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-04-13 23:37:02 +0800

    Create Journal.java

---
+++ Journal.java
@@ -0,0 +1,11 @@
+public class Journal implements LibraryResource{
+    private String title;
+
+    public Journal(String title){
+        this.title = title;
+    }
+    @Override
+    public String getTitle(){
+        return title;
+    }
+}
