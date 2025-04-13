df2af45b38ee73d9f400cecd86a38c03a851132f
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-04-13 23:37:49 +0800

    Create Capstone.java

---
+++ Capstone.java
@@ -0,0 +1,11 @@
+public class Capstone implements LibraryResource{
+    private String title;
+
+    public Capstone(String title){
+        this.title = title;
+    }
+    @Override
+    public String getTitle(){
+        return title;
+    }
+}
